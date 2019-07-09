4befd44f82c7c6782162c4323cc02fee7d2963d3
Juan Jimenez <jimenez.f.juan.m@gmail.com>
2019-07-09 01:30:11 -0400

    Events, props, router and more

---
+++ src/components/repos/RepoItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const RepoItem = ({ repo }) => {
+  const { html_url, name } = repo;
+  return (
+    <div className='card'>
+      <h3>
+        <a href={html_url} target='_blank' rel='noopener noreferrer'>
+          {name}
+        </a>
+      </h3>
+    </div>
+  );
+};
+
+RepoItem.propTypes = {
+  repo: PropTypes.object.isRequired,
+};
+
+export default RepoItem;
